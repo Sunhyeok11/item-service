92206e3ae1ba64c985ecd8ee8674d54a7b8fa66f
SunHyeok <cghhld@naver.com>
2024-03-05 16:59:45 +0900

    상품 수정

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -37,40 +37,23 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
-//    @PostMapping("/add")
-    public String addItemV1(@RequestParam("itemName") String itemName,
-                       @RequestParam("price") int price,
-                       @RequestParam("quantity") Integer quantity,
-                       Model model) {
-
-        Item item = new Item();
-        item.setItemName(itemName);
-        item.setPrice(price);
-        item.setQuantity(quantity);
-
-        itemRepository.save(item);
-
-        model.addAttribute("item", item);
-
-        return "basic/item";
-    }
-
-//    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item) {
-        itemRepository.save(item);
-        return "basic/item";
-    }
-
-    //@PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item, Model model) {
-        itemRepository.save(item);
-        return "basic/item";
-    }
-
     @PostMapping("/add")
-    public String addItemV4(Item item) {
+    public String addItemV5(Item item) {
         itemRepository.save(item);
-        return "basic/item";
+        return "redirect:/basic/items/" + item.getId();
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable("itemId") Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable("itemId") Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @PostConstruct
